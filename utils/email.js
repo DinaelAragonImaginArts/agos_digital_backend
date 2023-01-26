21b538329fd0146638f09a254081b3055fddebdb
Dinael Aragon <84431313+DinaelAragonImaginArts@users.noreply.github.com>
2023-01-25 16:59:40 -0800

    Add files via upload

---
+++ utils/email.js
@@ -0,0 +1,27 @@
+import sgMail from '@sendgrid/mail';
+
+export const envio = async (datos) => {
+  const { nombre, correo, telefono, detalles } = datos;
+  const info = {
+    from: `${correo}`,
+    to: 'info@agos.mx',
+    subject: `Nombre: ${nombre}`,
+    text: `Correo: ${correo}`,
+    html: `
+    <p>Nombre: ${nombre}</p>
+    <p>CORREO: ${correo}</p>
+    <p>TELÉFONO: ${telefono}</p>
+    <p>CUÉNTANOS TU VISIÓN: ${detalles}</p>
+    `
+  };
+  sgMail.setApiKey(process.env.SENDGRID_API_KEY)
+  sgMail
+    .send(info)
+    .then(() => {
+      console.log('Email sent')
+    })
+    .catch((error) => {
+      console.error(error)
+    })
+};
+
